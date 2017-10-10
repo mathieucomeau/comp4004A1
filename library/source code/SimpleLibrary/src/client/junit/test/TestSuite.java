a2e5eb3f4bbffe97d7ad0f6eb5d0acf1aa65ca32
Mathieu Comeau <Mathieu@172.17.216.83>
2017-10-10 18:07:10 -0400

    Collect Fine Test Case 1 Failed

--- library/source code/SimpleLibrary/src/client/junit/test/TestSuite.java
+++ library/source code/SimpleLibrary/src/client/junit/test/TestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TitleTest.class })
+@SuiteClasses({ TitleTest.class, ItemTest.class, UserTest.class })
 public class TestSuite {
 
 }
